ec52f2c52dffd2e85b3acd4572ff310865039f4f
Svetlana <khabarova09@rambler.ru>
2023-04-04 08:17:09 +0300

    сортировка строк по убыванию двумерного массива на 04.04.2023

---
+++ SortRows/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задать двумерный массив.
+//Упорядочить по убыванию элементы каждой строки.
+//напр: вход 1 4 7 2  выход 7 4 2 1
+//           5 9 2 3        9 5 3 2
+//           8 4 2 4        8 4 4 2
+Console.Write($"Введите кол-во строк ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Введите кол-во столбцов ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void FillArray(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+    for (int j = 0; j < arr.GetLength(1); j++)
+      arr[i, j] = new Random().Next(0, 10);
+
+}
+
+void PrintArray(int[,] ar)
+{
+  for (int i = 0; i < ar.GetLength(0); i++)
+  {
+    for (int j = 0; j < ar.GetLength(1); j++)
+      Console.Write($"{ar[i, j],2}\t");
+    Console.WriteLine();
+  }
+}
+
+void SortRows(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      int min = j;
+      for (int k = j + 1; k < array.GetLength(1); k++)
+      {
+        if (array[i, k] < array[i, min]) min = k;
+      }
+
+      int temp = array[i, j];
+      array[i, j] = array[i, min];
+      array[i, min] = temp;
+    }
+  }
+}
+
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+SortRows(array);
+PrintArray(array);
